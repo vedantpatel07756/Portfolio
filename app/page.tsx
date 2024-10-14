cbf2521e6c7725b8cfc00b255f787127539134b4
Vedant Patel <vedantpatel1827@gmail.com>
2024-10-14 12:57:49 +0530

    apos

--- app/page.tsx
+++ app/page.tsx
@@ -135,10 +135,10 @@ export default function Home() {
         Questions, Ideas, or Just a Chat?{" "} <br />
         <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
           <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-            <span className="">I'm All Ears!</span>
+            <span className="">I&apos;m All Ears!</span>
           </div>
           <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-            <span className="">I'm All Ears!</span>
+            <span className="">I&apos;m All Ears!</span>
           </div>
         </div>
 
